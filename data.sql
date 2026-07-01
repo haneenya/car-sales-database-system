84987dff0093a2f185b3bf7872c16c429840cc49
HaneenYaghi <155544273+haneenya@users.noreply.github.com>
2026-07-01 22:54:52 +0300

    Add sample data for database tables

---
+++ data.sql
@@ -0,0 +1,10 @@
+insert into Cars 
+values (101, 'Mercedes-Benz C-Class', 2023, 'Germany', 65000.000, 'Black', 250, 6, 350, 60),
+       (102, 'Mercedes-Benz E-Class', 2022, 'Germany', 45000.00, 'White', 260, 6, 400, 50),
+       (103, 'Mercedes-Benz S-Class', 2021, 'Germany', 35000.00, 'Silver', 280, 8, 500, 70),
+       (104, 'Mercedes-Benz GLC', 2024, 'Germany', 95000.00, 'Blue', 240, 6, 380, 45) ,
+   (109, 'Mercedes-Benz CLA-Class', 2023, 'Germany', 38000.00, 'Blue', 230, 4, 280, 58),
+       (110, 'Mercedes-Benz EQS', 2025, 'Germany', 130000.00, 'Germany', 250, 6, 550, 100),
+       (111, 'Mercedes-Benz S-Class', 2023, 'Germany', 120000.00, 'Gray', 290, 8, 700, 80),
+       (112, 'Mercedes-Benz C-Class', 2022, 'Germany', 42000.00, 'Green', 230, 6, 320, 65);
+ 
