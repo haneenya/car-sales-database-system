e6879808fc74708e829665ba7cdb3c2df9ef6e72
HaneenYaghi <155544273+haneenya@users.noreply.github.com>
2026-07-01 22:52:48 +0300

    Add Cars table schema
    Initial implementation of Cars table in SQL database

---
+++ schema.sql
@@ -0,0 +1,12 @@
+create table Cars (
+Car_ID int primary key ,
+Car_Type varchar(30) ,
+Manufacturer_Year int  ,
+Export_Country varchar(30) ,
+Price decimal(10, 2) ,
+Color varchar(20) ,
+Speedometer int ,
+Cylinders int ,
+Torque int  ,
+Fuel_Range int  
+)
